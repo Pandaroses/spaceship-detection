4536e5cb9871016fc7ec888be5431ad393a91447
Gosha Tnimov <73164974+Pandaroses@users.noreply.github.com>
2023-07-13 15:36:25 +0100

    Create index.py

---
+++ index.py
@@ -0,0 +1,20 @@
+def on_received_number(cb):
+    global count
+     if (radio.receivedPacket(RadioPacketProperty.SignalStrength) >= strength) {
+     basic.showNumber(radio.receivedPacket(RadioPacketProperty.SignalStrength))
+     radio.sendNumber(id2)
+     music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
+     }
+radio.on_received_number(on_received_number)
+
+
+strength = -42
+radio.set_transmit_power(7)
+radio.set_group(10)
+id2 = Math.floor(Math.random() * 9999)
+radio.on()
+
+def on_forever():
+    radio.send_number(id2)
+    basic.pause(350)
+basic.forever(on_forever)
